43b70d57b7b4f0337091b52a235a04fd2ad43729
Prajwal A <prajwalanagani@gmail.com>
2023-11-07 14:07:56 +0530

    Add the rest of the function required

--- my_autograd/core.py
+++ my_autograd/core.py
@@ -90,3 +90,27 @@ class Value:
         # For each node in the topological sort from the end, run the backward function
         for v in reversed(topo):
             v._backward()
+
+    def __neg__(self): # -self
+        return self * -1
+
+    def __radd__(self, other): # other + self
+        return self + other
+
+    def __sub__(self, other): # self - other
+        return self + (-other)
+
+    def __rsub__(self, other): # other - self
+        return other + (-self)
+
+    def __rmul__(self, other): # other * self
+        return self * other
+
+    def __truediv__(self, other): # self / other
+        return self * other**-1
+
+    def __rtruediv__(self, other): # other / self
+        return other * self**-1
+
+    def __repr__(self):
+        return f"Value(data={self.data}, grad={self.grad})"
